71456c62f74e17af2234b8835a37ff65039062e1
João Eduardo <joaoeduardoj920@gmail.com>
2023-03-24 09:25:12 -0300

    feat: adding news tecnologias in hearder

--- src/components/Hearder/index.tsx
+++ src/components/Hearder/index.tsx
@@ -1,83 +1,76 @@
-import * as S from './style';
+import * as S from "./style";
 
-import curriculo  from '../../download/JoaoE.pdf';
+import curriculo from "../../download/JoaoE.pdf";
 
-import { TypeAnimation } from 'react-type-animation';
-import { useState } from 'react';
+import { TypeAnimation } from "react-type-animation";
+import { useState } from "react";
 
-import { theme } from '../../style/themes';
+import { theme } from "../../style/themes";
 
-import { ButtoDownload } from '../Buttons';
-import { BiCodeAlt } from 'react-icons/bi';
+import { ButtoDownload } from "../Buttons";
+import { BiCodeAlt } from "react-icons/bi";
 
-import { useAppSelector } from '../../store/hookes';
+import { useAppSelector } from "../../store/hookes";
 
 export const Hearder = () => {
-	const [nameOne, setNameOne] = useState(false);
-	const [nameTwo, setNameTwo] = useState(false);
-	const { colorOn } = useAppSelector(item => item.menu);
+  const [nameOne, setNameOne] = useState(false);
+  const [nameTwo, setNameTwo] = useState(false);
+  const { colorOn } = useAppSelector((item) => item.menu);
 
-	return (
-		<S.Conteiner data-aos="fade-down">
-			<S.Conteinertext coloOn={colorOn}>
-				<TypeAnimation 
-					sequence={[
-						'Olá, Sou o',
-						1000,
-						() => setNameOne(true)
-					]}
-					wrapper = "h2"
-					cursor = {false}
-					className="textOne"
-				/>
-				{
-					nameOne && (
-						<TypeAnimation 
-							sequence={[
-								'João Eduardo :)',
-								1000,
-								() => setNameTwo(true)
-							]}
-							wrapper = "h2"
-							cursor = {false}
-							style = {{ color: theme.colors.purple_200 }}
-							className="textTwo"
-						/>
-					)
-				}
+  return (
+    <S.Conteiner data-aos="fade-down">
+      <S.Conteinertext coloOn={colorOn}>
+        <TypeAnimation
+          sequence={["Olá, Sou o", 1000, () => setNameOne(true)]}
+          wrapper="h2"
+          cursor={false}
+          className="textOne"
+        />
+        {nameOne && (
+          <TypeAnimation
+            sequence={["João Eduardo :)", 1000, () => setNameTwo(true)]}
+            wrapper="h2"
+            cursor={false}
+            style={{ color: theme.colors.purple_200 }}
+            className="textTwo"
+          />
+        )}
 
-				{
-					nameTwo && (
-						<TypeAnimation 
-							sequence={[
-								'Desenvolvedor front end',
-								1000,
-								'Tecnologia que utilizo',
-								1000,
-								'HTML5 | CSS3 | JavaScript',
-								3000,
-								'TypeScript | React.js | Sass',
-								3000,
-								'Redux | Context APi | API',
-								3000,
-								'Styled-Components | Git e GitHub',
-								3000
-                                
-							]}
-							wrapper = "h2"
-							cursor = {true}
-							repeat = {Infinity}
-							style = {{ color: '#828282' }}
-							className="text3"
-						/>
-					)
-				}
+        {nameTwo && (
+          <TypeAnimation
+            sequence={[
+              "Desenvolvedor Full Stack",
+              1000,
+              "Tecnologia que utilizo",
+              1000,
+              "HTML5 | CSS3 | JavaScript",
+              3000,
+              "TypeScript | React.js | Sass",
+              3000,
+              "NextJs | StoryBook | Jest",
+              3000,
+              "Redux | Context APi | API REST",
+              3000,
+              "Styled-Components | Git e GitHub",
+              3000,
+              "NodeJs | MongoDb | SQL",
+              3000,
+              "Docker | ExpressJs | Swagger",
+              3000,
+            ]}
+            wrapper="h2"
+            cursor={true}
+            repeat={Infinity}
+            style={{ color: "#828282" }}
+            className="text3"
+          />
+        )}
 
-				<ButtoDownload link={curriculo} text='Download CV' />
-			</S.Conteinertext>
-			<S.ConteinerIcon>
-				<BiCodeAlt />
-			</S.ConteinerIcon>
-		</S.Conteiner>
-	);
+        <ButtoDownload link={curriculo} text="Download CV" />
+      </S.Conteinertext>
+      <S.ConteinerIcon>
+        <BiCodeAlt />
+      </S.ConteinerIcon>
+    </S.Conteiner>
+  );
 };
--- src/components/Hearding/index.tsx
+++ src/components/Hearding/index.tsx
@@ -1,17 +1,13 @@
-import * as S from './style';
+import * as S from "./style";
 
-import { useAppSelector } from '../../store/hookes';
+import { useAppSelector } from "../../store/hookes";
 
 type Props = {
-    text: string
-}
+  text: string;
+};
 
 export const Hearding = ({ text }: Props) => {
-	const { colorOn } = useAppSelector(item => item.menu);
+  const { colorOn } = useAppSelector((item) => item.menu);
 
-	return (
-		<S.Conteiner colorOn={colorOn}>
-			{text}
-		</S.Conteiner>
-	);
+  return <S.Conteiner colorOn={colorOn}>{text}</S.Conteiner>;
 };
