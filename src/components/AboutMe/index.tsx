66e221888aa8f55b2836eaecec38385db3b5522b
João Eduardo <joaoeduardoj920@gmail.com>
2023-03-24 09:18:19 -0300

    feat:adding new aboutMe

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -1,21 +1,28 @@
-import { Hearding } from '../Hearding';
-import * as S from './style';
+import { Hearding } from "../Hearding";
+import * as S from "./style";
 
 export const AboutMe = () => {
-
-
-	return (
-		<S.Conteiner data-aos='fade-right' id='sobre'>
-			<Hearding text='Sobre mim'/>
-			<S.ConteinerText>
-				<h3>Teresina, PI</h3>
-				<S.Paragrafo>
-                    Olá, me chamo João Eduardo e sou um apaixonado por tecnologia. Meu primeiro contato com programação, foi quando vi o meu primo fazer uma calculadora com Python. Maravilhado com tudo aquilo, quis logo aprender sobre.
-				</S.Paragrafo>
-				<S.Paragrafo>
-                    Hoje, já tenho conhecimento em algumas tecnologias voltadas para o Front-end. E continuo meus estudos, tendo sempre em mente que estamos em constante aprendizado.
-				</S.Paragrafo>
-			</S.ConteinerText>
-		</S.Conteiner>
-	);
+  return (
+    <S.Conteiner data-aos="fade-right" id="sobre">
+      <Hearding text="Sobre mim" />
+      <S.ConteinerText>
+        <h3>Teresina, PI</h3>
+        <S.Paragrafo>
+          Olá, meu nome é João Eduardo e sou apaixonado por tecnologia. Tive meu
+          primeiro contato com programação quando meu primo me mostrou como
+          criar uma calculadora com Python. Fiquei fascinado com a ideia de
+          poder criar algo tão incrível e decidi aprender mais sobre
+          programação.
+        </S.Paragrafo>
+        <S.Paragrafo>
+          Atualmente, tenho conhecimentos em algumas tecnologias tanto de
+          front-end quanto de back-end e continuo estudando constantemente,
+          ciente de que a aprendizagem é uma jornada contínua. Busco sempre me
+          aprimorar e descobrir novas tecnologias para continuar evoluindo como
+          desenvolvedor. Estou animado para continuar aprendendo e crescendo no
+          mundo da tecnologia!
+        </S.Paragrafo>
+      </S.ConteinerText>
+    </S.Conteiner>
+  );
 };
