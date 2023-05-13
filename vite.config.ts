3e507f938478c5439adb4f5798a501b6ff17c297
João Eduardo <joaoeduardoj920@gmail.com>
2023-05-13 11:56:12 -0300

    feat: adding new  curiculo

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	test: {
-		globals: true,
-		environment: 'jsdom'
-	}
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
 });
